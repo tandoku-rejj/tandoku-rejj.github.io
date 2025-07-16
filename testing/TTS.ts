cb38a0d4fa0091e1fa0e37cc4e9516a3aeb4f89e
jerome.rayos <rejjtandoku@gmail.com>
2025-07-16 15:26:00 +0800

    working word08

--- testing/TTS.ts
+++ testing/TTS.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import fs from "fs"
 import path from "path";
-import { word07 } from "./words";
+import { word08 } from "./words";
 // npx tsx testing/TTS
 // ✅ Dictionary configs
 const DICTIONARIES: Record<string, { key: string; label: string }> = {
@@ -73,16 +73,16 @@ async function fetchDefinition(word: string): Promise<void> {
 
 // ✅ Main execution
 async function run(): Promise<void> {
-    for (const word of word07) {
+    for (const word of word08) {
         await fetchDefinition(word);
     }
 
     const outputDir = "testing/generatedFiles";
     fs.mkdirSync(outputDir, { recursive: true });
 
-    fs.writeFileSync(path.join(outputDir, "word07.txt"), humanReadable);
+    fs.writeFileSync(path.join(outputDir, "word08.txt"), humanReadable);
 
-    console.log("✅ Done. Files saved: word07.json and word07.txt");
+    console.log("✅ Done. Files saved: word08.json and word08.txt");
 }
 
 run();
